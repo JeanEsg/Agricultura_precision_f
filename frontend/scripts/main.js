1c9ad5fe1a8d689bfcc10503002d870ca71c2f79
JeanEsg <160681687+JeanEsg@users.noreply.github.com>
2024-11-06 19:38:39 -0500

    Fix method post and fix data results

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,64 +1,105 @@
+let sensorData = null;
+
 document.getElementById('sensor-data').addEventListener('click', () => {
     fetch('http://127.0.0.1:8002/sensor-data') // Cambiar el puerto a conveniencia
         .then(response => response.json())
         .then(data => {
+            sensorData = {
+                temperatura: data.temperatura,
+                humedad_aire: data.humedad_aire,
+                humedad_suelo: data.humedad_suelo
+            };
+
             const resultElement = document.getElementById('result');
             resultElement.innerHTML = `
-            
             <p>Temperatura: ${data.temperatura}°C</p>
-            <p>Humedad: ${data.humeda_aire}%</p>
-            <p>Humedad Relativa: ${data.humedad_suelo}%</p>
+            <p>Humedad Relativa del Aire: ${data.humedad_aire}%</p>
+            <p>Humedad del Suelo: ${data.humedad_suelo}%</p>
         `;
         }) 
         .catch(error => console.error('Error:', error));
 });
 
 document.getElementById('save').addEventListener('click', () => {
-    fetch('http://127.0.0.1:8002/save') // Cambiar el puerto a conveniencia
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Error: ${response.statusText}`);
-            }
-            return response.text();
-        })
-        .then(data => {
-            alert(data) // Mostrar la cadena directamente
-        })
-        .catch(error => console.error('Error:', error));
+    if (!sensorData){
+        alert('Por favor, primero obtén los datos de los sensores');
+        return;
+    }
+    fetch('http://127.0.0.1:8002/save',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(sensorData)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Error: ${response.statusText}`);
+        }
+        return response.text();
+    })
+    .then(data => {
+        alert(data) 
+    })
+    .catch(error => console.error('Error:', error));
 });
 
-
 document.getElementById('cantidad-agua-litros').addEventListener('click', () => {
-    fetch('http://127.0.0.1:8002/cantidad') // Cambiar el puerto a conveniencia
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Error: ${response.statusText}`);
-            }
-            return response.text();
+    const dataEntrada = document.getElementById('result2').value;
+    const datos = dataEntrada.split(",").map(x => parseFloat(x.trim()));
+    if (datos.length === 4){
+        const AguaRequest = {
+            area: datos[0],
+            temperatura: datos[1],
+            humedad_aire: datos[2],
+            humedad_suelo: datos[3]
+        };
+        fetch('http://127.0.0.1:8002/cantidad',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(AguaRequest)
         })
-        .then(data => {
-            document.getElementById('result2').textContent = data; // Mostrar la cadena directamente
-        })
-        .catch(error => console.error('Error:', error));
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`Error: ${response.statusText}`);
+                }
+                return response.text();
+            })
+            .then(data => {
+                document.getElementById('result2').textContent = data; // Mostrar la cadena directamente
+            })
+            .catch(error => console.error('Error:', error));
+    } else {
+        alert('Por favor, ingresa exactamente cuatro valores separados por comas.');
+    }
 });
 
 document.getElementById('generar-recomendacion').addEventListener('click', () => {
-    fetch('http://127.0.0.1:8002/generar_respuesta') // Cambiar el puerto a conveniencia
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Error: ${response.statusText}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            let res = "";            
-            res += `Recomendacion:\n ${data.response}\n`;
-
-            const cadenaConSaltos = res.replace(/\n/g, '<br>');
-            document.getElementById('result3').innerHTML = cadenaConSaltos;
-            
-
-        })
-        .catch(error => console.error('Error:', error));
+    const promptText = document.getElementById('result3').value;
+    const promptRequest = {
+        prompt: promptText
+    };
+    fetch('http://127.0.0.1:8002/generar_respuesta', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(promptRequest)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Error: ${response.statusText}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        let res = "";            
+        res += `Recomendacion:\n ${data.response}\n`;
+        const cadenaConSaltos = res.replace(/\n/g, '<br>');
+        document.getElementById('result3').innerHTML = cadenaConSaltos;
+    })
+    .catch(error => console.error('Error:', error));
 });
 
